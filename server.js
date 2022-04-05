dc085d37161db02c1f66d6e42b1f37d6d75ab9e8
Nick Melanson <nickm890@gmail.com>
2022-04-05 17:45:24 -0400

    added candidated creation

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const res = require("express/lib/response");
 const mysql = require("mysql2");
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -20,9 +21,37 @@ const db = mysql.createConnection(
   console.log("Connected to the election database.")
 );
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-  console.log(rows);
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+                VALUES (?,?,?,?)`;
+const params = [1, "Ronald", "Firbank", 1];
+
+db.query(sql, params, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
 });
+
+// Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+
+// GET a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(row);
+// });
+
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//   console.log(rows);
+// });
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
